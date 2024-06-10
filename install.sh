43baf6292bddcb02e607ba472fa4fc5872aa8279
inmymum <114446605+inmymum@users.noreply.github.com>
2024-06-10 17:35:20 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo mkdir /etc/sddm.conf.d/
 rm /home/$USER/.config/hypr/hyprland.conf
 echo "Downloading apps"
 echo "  Downloading packages"
-sudo pacman -S --noconfirm hyprlock hypridle unzip
+sudo pacman -S --noconfirm hyprlock hypridle unzip tlp
 echo "  Installed packages"
 echo "  Downloading browser (thorium)"
 wget -q https://github.com/$(curl -s https://github.com/Alex313031/Thorium/releases|grep '<a href="/Alex313031/thorium/releases'|grep SSE3.zip|awk '{print substr($0,18,93)}') -P /home/$USER/apps/thorium
@@ -17,8 +17,12 @@ echo "  hypridle"
 curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/.config/hypr/hypridle.conf | tee /home/$USER/.config/hypr/hypridle.conf
 echo "  hyprlock"
 curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/.config/hypr/hyprlock.conf | tee /home/$USER/.config/hypr/hyprlock.conf
+echo "  tlp"
+sudo systemctl enable tlp
+curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/etc/tlp.conf | sudo tee /etc/tlp.conf
 echo "  bootloader"
 curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/boot/loader/loader.conf | sudo tee /boot/loader/loader.conf
 echo "  autologin"
-curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/sddm/autologin.conf | sudo tee /etc/sddm.conf.d/autologin.conf
+curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/etc/sddm.conf.d/autologin.conf | sudo tee /etc/sddm.conf.d/autologin.conf
 echo "Installation complete"
+reboot
