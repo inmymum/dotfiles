5fc039b5a1bd83aaa888c8e8602b10b3485f3bf1
inmymum <114446605+inmymum@users.noreply.github.com>
2024-06-10 17:52:13 +0000

    Added additional bootloader configs
    Enabled silent boot mode - no more systemd info on boot screen

--- install.sh
+++ install.sh
@@ -12,17 +12,18 @@ unzip -qq $(ls /home/$USER/apps/thorium/) -d /home/$USER/apps/thorium
 echo "  Installed browser into /home/$USER/apps/thorium/"
 echo "Setting up configuration files"
 echo "  hyprland"
-curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/.config/hypr/hyprland.conf | tee /home/$USER/.config/hypr/hyprland.conf
+curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/.config/hypr/hyprland.conf | tee /home/$USER/.config/hypr/hyprland.conf > /dev/null
 echo "  hypridle"
-curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/.config/hypr/hypridle.conf | tee /home/$USER/.config/hypr/hypridle.conf
+curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/.config/hypr/hypridle.conf | tee /home/$USER/.config/hypr/hypridle.conf > /dev/null
 echo "  hyprlock"
-curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/.config/hypr/hyprlock.conf | tee /home/$USER/.config/hypr/hyprlock.conf
+curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/.config/hypr/hyprlock.conf | tee /home/$USER/.config/hypr/hyprlock.conf > /dev/null
 echo "  tlp"
 sudo systemctl enable tlp
-curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/etc/tlp.conf | sudo tee /etc/tlp.conf
+curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/etc/tlp.conf | sudo tee /etc/tlp.conf > /dev/null 
 echo "  bootloader"
-curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/boot/loader/loader.conf | sudo tee /boot/loader/loader.conf
+sudo sed -i "/options/s/$/ silent/" /boot/loader/entries/$(ls /boot/loader/entries/|grep -v fallback)
+curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/boot/loader/loader.conf | sudo tee /boot/loader/loader.conf > /dev/null
 echo "  autologin"
-curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/etc/sddm.conf.d/autologin.conf | sudo tee /etc/sddm.conf.d/autologin.conf
+curl -s https://raw.githubusercontent.com/inmymum/dotfiles/main/etc/sddm.conf.d/autologin.conf | sudo tee /etc/sddm.conf.d/autologin.conf > /dev/null
 echo "Installation complete"
 reboot
