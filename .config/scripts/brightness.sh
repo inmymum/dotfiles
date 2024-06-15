2252d2b67f57dcbeea322cffcd04036842d1da02
inmymum <114446605+inmymum@users.noreply.github.com>
2024-06-15 23:25:25 +0000

    Update brightness.sh

--- .config/scripts/brightness.sh
+++ .config/scripts/brightness.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-if [[ $1 ]]; then echo "$(($(cat /sys/class/backlight/intel_backlight/brightness)+$1))"|sudo tee /sys/class/backlight/intel_backlight/brightness>/dev/null; fi
+if [[ -z "$1" || "$1" != "get" ]]; then
+ echo "$(($(cat /sys/class/backlight/intel_backlight/brightness)+$1))"|sudo tee /sys/class/backlight/intel_backlight/brightness>/dev/null
+else 
+ cat /sys/class/backlight/intel_backlight/brightness
+fi
