8842bd64464882197b7863c4a34c49c51a2cadbd
inmymum <114446605+inmymum@users.noreply.github.com>
2024-06-16 00:59:43 +0000

    Update brightness.sh

--- .config/scripts/brightness.sh
+++ .config/scripts/brightness.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [[ -z "$1" || "$1" != "get" ]]; then
- echo "$(($(cat /sys/class/backlight/intel_backlight/brightness)+$1))"|sudo tee /sys/class/backlight/intel_backlight/brightness>/dev/null
-else 
- cat /sys/class/backlight/intel_backlight/brightness
+if [[ -z "$1" || "$1" != "-nopwd" ]]; then
+ echo 10210813 | sudo -S sh -c 'echo "$(($(cat /sys/class/backlight/intel_backlight/brightness)+500))"|sudo tee /sys/class/backlight/intel_backlight/brightness>/dev/null 2>&1'
+else
+ echo "$(($(cat /sys/class/backlight/intel_backlight/brightness)+500))"|sudo tee /sys/class/backlight/intel_backlight/brightness>/dev/null
 fi
