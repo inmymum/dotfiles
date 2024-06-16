50a3ceb1d057ca026716f6e865025aa8fb825ec3
inmymum <114446605+inmymum@users.noreply.github.com>
2024-06-16 01:15:14 +0000

    Integrate brightness.sh to hyprland.conf

--- .config/scripts/brightness.sh
+++
@@ -1,5 +0,0 @@
-if [[ -z "$1" || "$1" != "get" ]]; then
- echo 10210813 | sudo -S sh -c 'echo "$(($(cat /sys/class/backlight/intel_backlight/brightness)+500))"|sudo tee /sys/class/backlight/intel_backlight/brightness>/dev/null 2>&1'
-else
- cat /sys/class/backlight/intel_backlight/brightness
-fi
