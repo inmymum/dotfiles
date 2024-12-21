9ac5a48526d2a497b2676a86aa2c47875984e958
inmymum <114446605+inmymum@users.noreply.github.com>
2024-12-20 21:40:15 -0600

    Create open_music.sh

---
+++ .local/share/scripts/open_music.sh
@@ -0,0 +1,1 @@
+/usr/bin/chromium --new-window --profile-directory=Default --enable-features=UseOzonePlatform --ozone-platform=wayland --app="https://music.amazon.com/my/library" --disable-crash-reporter 2&>1
