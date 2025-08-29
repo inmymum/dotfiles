43d72ce40a54951bc9698275008b0c8d8dda624a
inmymum <114446605+inmymum@users.noreply.github.com>
2025-08-29 13:00:48 -0500

    Modify to work with all browsers

--- .local/share/scripts/fuzzel_with_web_search.sh
+++ .local/share/scripts/fuzzel_with_web_search.sh
@@ -1,1 +1,1 @@
-output=$(fuzzel 2>&1); if [[ -n "$output" ]]; then mercury-browser --search "$(echo $output|awk -F': ' '{print $3}')"; fi
+output=$(fuzzel 2>&1); if [[ -n "$output" ]]; then xdg-open "$(echo $output|awk -F': ' '{print $3}')"; fi
